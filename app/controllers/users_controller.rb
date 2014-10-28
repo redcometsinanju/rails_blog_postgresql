fb7a94728f6065c32e9c8b3f1a8cd807decd92b5
Coen <coenching@gmail.com>
2014-10-27 18:27:02 -0700

    profile and edit profile, comment route back to post page, now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,29 @@ class UsersController < ApplicationController
 		else
 			redirect_to new_user_path
 		end
-
 	end
+
+	# Finds the user depending on the :id of the user, from users/2, will return user 2
+	# To set the user to only be the current user, @user = User.find(sessions[:current_user_id])
+	def show
+		@user = User.find(params[:id])
+		# @current_user = User.find(sessions[:current_user_id])
+	end
+
+	def edit
+  end
+
+  def update
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to posts_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 	def destroy
 	end
