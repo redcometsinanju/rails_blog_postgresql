4b939b7617e4f11ab5fb356548bdf886c7f71bec
Coen <coenching@gmail.com>
2014-10-29 09:46:54 -0700

    user profile photo upload working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+  mount_uploader :image, ImageUploader
+
 	has_many :posts
 
 	validates :email, uniqueness: true
---
+++ db/migrate/20141029162156_add_photo_to_user.rb
@@ -0,0 +1,5 @@
+class AddPhotoToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :image, :string
+  end
+end
