1888417120601c3d8cea67b13fd47cc8bbe8800c
Coen <coenching@gmail.com>
2014-10-29 10:27:33 -0700

    add_column url to post, now working

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
 	validates :title, presence: true
-	validates :description, presence: true
+	validates :url, presence: true
+	# validates :description, presence: true
 
 end
