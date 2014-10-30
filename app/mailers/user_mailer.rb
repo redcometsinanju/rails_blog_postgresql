8d146bedc56649742b1c6dc745658e0892d3cbef
Coen <coenching@gmail.com>
2014-10-30 11:08:06 -0700

    changed welcome email url to heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     email_with_name = "#{@user.username} <#{@user.email}>"
-    @url  = 'https://moodly-app.herokuapp.com/'
+    @url  = 'https://moodly-app.herokuapp.com/users/new'
     mail(to: email_with_name, subject: 'Welcome to Moodly!')
   end
 end
+'http://example.com/login'
